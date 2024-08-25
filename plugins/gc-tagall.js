7e2f87bbf76f1413ba0fb1b426f6d532630e658e
Xnnv2 <toscren0@gmail.com>
2024-08-24 21:47:48 -0500

    xddd

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,19 +4,19 @@ let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, comma
   throw false
   }
   let pesan = args.join` `
-  let oi = `𓍢ִ໋  𝙈𝙪𝙨𝙖 𝘽𝙤𝙩 🧡
- ${pesan}`
-  let teks = `• 🛍️ Adquiere el bot con
-wa.me/+51944026510*
-   ${oi}\n\n↳ 𝗘𝘁𝗶𝗾𝘂𝗲𝘁𝗮: *\n`
+  let oi = `*𝗠𝗲𝗻𝘀𝗮𝗷𝗲:* ${pesan}`
+  let teks = `𓍢ִ໋  𝙈𝙪𝙨𝙖 𝘽𝙤𝙩 🧡
+
+* 🛍️ Adquiere el bot con
+wa.me/+51944026510\n\n↳ ${oi}\n\n↳ *ETIQUETA:*\n`
   for (let mem of participants) {
-  teks += `• 🍥 @${mem.id.split('@')[0]}\n`}
-  teks += `𝙈𝙪𝙨𝙖 𝘽𝙤𝙩 🩷`
+  teks += `･🍹 @${mem.id.split('@')[0]}\n`}
+  teks += `⇢ 𝗠𝗨𝗦𝗔𝗕𝗢𝗧`
   conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
   }
   handler.help = ['tagall <mesaje>','invocar <mesaje>']
   handler.tags = ['group']
-  handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten)$/i
+  handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
   handler.admin = true
   handler.group = true
   export default handler
